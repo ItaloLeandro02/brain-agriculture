2a48ba6bd1f1d94cea836e4dd9b05f3b98097af1
Italo Leandro <italo_leandro@outlook.com>
2024-03-14 18:45:06 -0300

    refactor: formata arquivos de acordo com o eslint

--- src/infra/db/postgres/rural-producer-postgres-repository.ts
+++ src/infra/db/postgres/rural-producer-postgres-repository.ts
@@ -3,13 +3,13 @@ import type { AddRuralProducerRepository } from '@/data/protocols'
 
 export class RuralProducerPostgresRepository implements AddRuralProducerRepository {
   async add (params: AddRuralProducerRepository.Params): Promise<number> {
-		const ruralProducerTable = KnexHelper.getInstance('rural_producer')
+    const ruralProducerTable = KnexHelper.getInstance('rural_producer')
     const [result] = await ruralProducerTable
-		.insert({
-			name: params.name,
-			cpf_cnpj: params.cpfCnpj
-		})
-		.returning('id')
-		return result.id
+      .insert({
+        name: params.name,
+        cpf_cnpj: params.cpfCnpj
+      })
+      .returning('id')
+    return result.id
   }
 }
