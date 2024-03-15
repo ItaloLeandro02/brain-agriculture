3bed87ec3646ecec6db7ee44b833afb0b0a3a5ed
Italo Leandro <italo_leandro@outlook.com>
2024-03-15 02:45:23 -0300

    test: garante que CnpjValidatorAdapter retorne true caso o  Validator retorne true

--- tests/infra/validators/cnpj-validator-adapter.spec.ts
+++ tests/infra/validators/cnpj-validator-adapter.spec.ts
@@ -1,6 +1,14 @@
 import { cnpj as cnpjValidator } from 'cpf-cnpj-validator'
 import { CnpjValidatorAdapter } from '@/infra/validators'
 
+jest.mock('cpf-cnpj-validator', () => ({
+  cnpj: {
+    isValid: () => {
+      return true
+    }
+  }
+}))
+
 const makeSut = (): CnpjValidatorAdapter => {
   return new CnpjValidatorAdapter()
 }
@@ -17,5 +25,9 @@ describe('CnpjValidator Adapter', () => {
     const sut = makeSut()
     jest.spyOn(cnpjValidator, 'isValid').mockReturnValueOnce(false)
     expect(sut.isValid('11.111.111/1111-11')).toBe(false)
+  })
+  test('Deve retornar true caso o Validator retorne true', () => {
+    const sut = makeSut()
+    expect(sut.isValid('31.307.371/0001-44')).toBe(true)
   })
 })
