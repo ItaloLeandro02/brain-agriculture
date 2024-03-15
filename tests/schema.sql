0890cdc59042bc25d05e67d85f49630c89da8bc8
Italo Leandro <italo_leandro@outlook.com>
2024-03-15 17:59:37 -0300

    test: adiciona schema da tabela farm

--- tests/schema.sql
+++ tests/schema.sql
@@ -5,3 +5,14 @@ create table brain_agriculture.rural_producer (
   name       varchar(100) not null,
   cpf_cnpj   varchar(100) not null
 );
+
+create table brain_agriculture.farm (
+  id				 						serial primary key,
+	rural_producer_id			serial not null,
+	name									varchar(100) not null,
+	city_name							varchar(100) not null,
+	state									char(2) not null,
+	total_area						numeric(5, 2) not null,
+	agricultural_area			numeric(5, 2) not null,
+	vegetation_area				numeric(5, 2) not null
+);
