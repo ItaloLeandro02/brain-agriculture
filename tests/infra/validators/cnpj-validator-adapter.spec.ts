56bc77cb9d12c4eb95a83cbb76423252f29fa70a
Italo Leandro <italo_leandro@outlook.com>
2024-03-15 02:42:50 -0300

    test: garante que CnpjValidatorAdapter chame o Validator com o cnpj correto

---
+++ tests/infra/validators/cnpj-validator-adapter.spec.ts
@@ -0,0 +1,16 @@
+import { cnpj as cnpjValidator } from 'cpf-cnpj-validator'
+import { CnpjValidatorAdapter } from '@/infra/validators'
+
+const makeSut = (): CnpjValidatorAdapter => {
+  return new CnpjValidatorAdapter()
+}
+
+describe('CnpjValidator Adapter', () => {
+  test('Deve chamar Validator com o cnpj corretor', () => {
+    const sut = makeSut()
+    const isValidSpy = jest.spyOn(cnpjValidator, 'isValid')
+    const cnpj = '64876764000128'
+    sut.isValid(cnpj)
+    expect(isValidSpy).toHaveBeenCalledWith(cnpj)
+  })
+})
