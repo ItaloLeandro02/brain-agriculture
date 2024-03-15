505a12822d42f6acc63ff4ca793b0b06aaa8f972
Italo Leandro <italo_leandro@outlook.com>
2024-03-15 01:16:03 -0300

    feat: garante que CnpjValidation retorne InvalidParamError caso CnpjValidator retorne falso

--- src/validation/validators/cnpj-validation.ts
+++ src/validation/validators/cnpj-validation.ts
@@ -1,5 +1,6 @@
 import type { CnpjValidator } from '@/validation/protocols'
 import type { Validation } from '@/presentation/protocols'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class CnpjValidation implements Validation {
   constructor (
@@ -9,6 +10,6 @@ export class CnpjValidation implements Validation {
 
   validate (input: any): Error {
     this.cnpjValidator.isValid(input[this.fieldName])
-    return null
+    return new InvalidParamError(this.fieldName)
   }
 }
