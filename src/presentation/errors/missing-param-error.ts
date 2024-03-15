873743c355e4b13b458a71418b3bb772ef1ae13f
Italo Leandro <italo_leandro@outlook.com>
2024-03-14 23:24:10 -0300

    refactor: adiciona prop name a MissinParamError

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,5 +1,6 @@
 export class MissinParamError extends Error {
   constructor (paramName: string) {
     super(`O parâmetro "${paramName}" deve ser informado`)
+    this.name = 'MissinParamError'
   }
 }
