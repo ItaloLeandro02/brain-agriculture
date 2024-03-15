cb8cde473b36d9b29890c3752777e38460122b9e
Italo Leandro <italo_leandro@outlook.com>
2024-03-15 01:43:48 -0300

    test: garante que TotalAreaValidation retorne InvalidParamError caso a soma das areas seja maior do que da area total

---
+++ tests/validation/validators/total-area-validation.spec.ts
@@ -0,0 +1,25 @@
+import { TotalAreaValidation } from '@/validation/validators'
+import { InvalidParamError } from '@/presentation/errors'
+
+const makeSut = (): TotalAreaValidation => {
+  return new TotalAreaValidation('totalArea', 'agriculturalArea', 'vegetationArea')
+}
+
+type Params = {
+  totalArea: number
+  agriculturalArea: number
+  vegetationArea: number
+}
+const mockParams = (): Params => ({
+  totalArea: 300,
+  agriculturalArea: 200,
+  vegetationArea: 100
+})
+
+describe('TotalArea Validation', () => {
+  test('Deve retornar InvalidParamError caso a soma da área agriculável com área de vegetação seja maior do que à área total', () => {
+    const sut = makeSut()
+    const error = sut.validate({ ...mockParams(), vegetationArea: 150 })
+    expect(error).toEqual(new InvalidParamError('totalArea'))
+  })
+})
