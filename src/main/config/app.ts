c6732a353c8105d081546be7339b46fabf65df94
Italo Leandro <italo_leandro@outlook.com>
2024-03-16 14:14:05 -0300

    feat: garante que o parser do body seja feito para o formato json

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import type { Express } from 'express'
+import { setupMiddlewares } from '@/main/config'
 
 export const setupApp = (): Express => {
   const app = express()
+  setupMiddlewares(app)
   return app
 }
