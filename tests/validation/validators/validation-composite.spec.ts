fcffcc8f04c2dca432f640e224169a48fc1a5e2d
Italo Leandro <italo_leandro@outlook.com>
2024-03-16 20:26:11 -0300

    test: garante que ValidationComposite retorne o primeiro error caso mais de uma validação falhe

--- tests/validation/validators/validation-composite.spec.ts
+++ tests/validation/validators/validation-composite.spec.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { ValidationComposite } from '@/validation/validators'
 import type { Validation } from '@/presentation/protocols'
-import { MissinParamError } from '@/presentation/errors'
+import { InvalidParamError, MissinParamError } from '@/presentation/errors'
 import { ValidationSpy } from '@/tests/validation/mocks'
 
 type SutTypes = {
@@ -27,5 +27,12 @@ describe('Validation Composite', () => {
     jest.spyOn(validationsSpy[1], 'validate').mockReturnValueOnce(new MissinParamError('field'))
     const error = sut.validate({ field: faker.lorem.word() })
     expect(error).toEqual(new MissinParamError('field'))
+  })
+  test('Deve retornar o primeiro erro caso mais de uma validação falhe', () => {
+    const { sut, validationsSpy } = makeSut()
+    jest.spyOn(validationsSpy[0], 'validate').mockReturnValueOnce(new InvalidParamError('another_field'))
+    jest.spyOn(validationsSpy[1], 'validate').mockReturnValueOnce(new MissinParamError('field'))
+    const error = sut.validate({ field: faker.lorem.word() })
+    expect(error).toEqual(new InvalidParamError('another_field'))
   })
 })
