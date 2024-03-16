5b3512837e6a802fd3f894ef1ebcc998498d7a77
Italo Leandro <italo_leandro@outlook.com>
2024-03-16 14:30:15 -0300

    test: garante que o cache seja desabilitado

---
+++ tests/main/middlewares/no-cache.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import { type Express } from 'express'
+import { setupApp } from '@/main/config'
+import { noCache } from '@/main/middlewares'
+
+let app: Express
+
+describe('No Cache Middleware', () => {
+  beforeAll(async () => {
+    app = setupApp()
+  })
+
+  test('Deve desabilitar cache', async () => {
+    app.get('/test_no_cache', noCache, (req, res) => res.send())
+
+    await request(app)
+      .get('/test_no_cache')
+      .expect('cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+      .expect('pragman', 'no-cache')
+      .expect('expires', '0')
+      .expect('surrogate-control', 'no-store')
+  })
+})
