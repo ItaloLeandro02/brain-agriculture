7e7c95b9fc9a9c8ef4287ef69f49ec20aa59b039
Italo Leandro <italo_leandro@outlook.com>
2024-03-16 21:19:06 -0300

    test: garante que StateValidation não retorne um error em caso de sucesso

--- tests/validation/validators/state-validation.spec.ts
+++ tests/validation/validators/state-validation.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { StateValidation } from '@/validation/validators'
 import { InvalidParamError } from '@/presentation/errors'
 
@@ -8,12 +9,17 @@ const makeSut = (length = 2): StateValidation => {
 describe('State Validation', () => {
   test('Deve retornar InvalidParamError caso o valor contenha mais do que letras', () => {
     const sut = makeSut()
-    const error = sut.validate({ state: 'B1' })
+    const error = sut.validate({ state: faker.string.uuid() })
     expect(error).toEqual(new InvalidParamError('state'))
   })
   test('Deve retornar InvalidParamError caso o valor contenha mais do que 2 caracteres', () => {
     const sut = makeSut()
-    const error = sut.validate({ state: 'BAA' })
+    const error = sut.validate({ state: faker.lorem.word(3) })
     expect(error).toEqual(new InvalidParamError('state'))
+  })
+  test('Não deve retornar um error em caso de sucesso', () => {
+    const sut = makeSut()
+    const error = sut.validate({ state: faker.lorem.word(2) })
+    expect(error).toBeFalsy()
   })
 })
