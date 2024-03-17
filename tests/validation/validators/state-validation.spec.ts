46a49d1f2693c68218e4f89f5a4e69beeca295fe
Italo Leandro <italo_leandro@outlook.com>
2024-03-16 21:01:39 -0300

    test: garante que StateValidation retorne um error caso o valor seja maior do que o definido

--- tests/validation/validators/state-validation.spec.ts
+++ tests/validation/validators/state-validation.spec.ts
@@ -1,14 +1,19 @@
 import { StateValidation } from '@/validation/validators'
 import { InvalidParamError } from '@/presentation/errors'
 
-const makeSut = (): StateValidation => {
-  return new StateValidation('state')
+const makeSut = (length = 2): StateValidation => {
+  return new StateValidation('state', length)
 }
 
 describe('State Validation', () => {
   test('Deve retornar InvalidParamError caso o valor contenha mais do que letras', () => {
     const sut = makeSut()
     const error = sut.validate({ state: 'B1' })
+    expect(error).toEqual(new InvalidParamError('state'))
+  })
+  test('Deve retornar InvalidParamError caso o valor contenha mais do que 2 caracteres', () => {
+    const sut = makeSut()
+    const error = sut.validate({ state: 'BAA' })
     expect(error).toEqual(new InvalidParamError('state'))
   })
 })
