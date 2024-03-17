5c15650e5721b1ef572c52db7b230921069e46d0
Italo Leandro <italo_leandro@outlook.com>
2024-03-16 21:01:28 -0300

    feat: garante que StateValidation retorne um error caso o valor seja maior do que o definido

--- src/validation/validators/state-validation.ts
+++ src/validation/validators/state-validation.ts
@@ -2,7 +2,10 @@ import type { Validation } from '@/presentation/protocols'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class StateValidation implements Validation {
-  constructor (private readonly fieldName: string) {}
+  constructor (
+    private readonly fieldName: string,
+    private readonly length: number
+  ) {}
 
   validate (input: any): Error {
     return new InvalidParamError(this.fieldName)
