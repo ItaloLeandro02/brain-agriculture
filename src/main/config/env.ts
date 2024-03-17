67499a68c17331665abc49046a85f1308a3da7b6
Italo Leandro <italo_leandro@outlook.com>
2024-03-17 19:15:42 -0300

    refactor: adiciona controle de conexão ao banco de acordo com o ambiente

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,5 @@ import 'dotenv/config'
 
 export const env = {
   port: process.env.PORT,
-  postgresUrl: process.env.POSTGRES_URL
+  postgresUrl: process.env.NODE_ENV === 'test' ? process.env.POSTGRES_URL_TEST : process.env.POSTGRES_URL
 }
