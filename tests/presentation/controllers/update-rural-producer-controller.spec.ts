c05b67eee514111c48dd56933d5a1a3c203f306d
Italo Leandro <italo_leandro@outlook.com>
2024-03-17 20:22:08 -0300

    test: garante que UpdateRuralProducerController chame Validation com os dados corretos

---
+++ tests/presentation/controllers/update-rural-producer-controller.spec.ts
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker'
+import { ValidationSpy } from '@/tests/presentation/mocks'
+import { UpdateRuralProducerController } from '@/presentation/controllers'
+
+type SutTypes = {
+  sut: UpdateRuralProducerController
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = new UpdateRuralProducerController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
+const mockRequest = (): UpdateRuralProducerController.Request => ({
+  id: faker.number.int({ min: 1, max: 100 }),
+  cpfCnpj: '852.415.280-08',
+  name: faker.person.fullName(),
+  farmName: faker.company.name(),
+  cityName: faker.location.city(),
+  state: 'BA',
+  totalArea: 200,
+  agriculturalArea: 100,
+  vegetationArea: 100,
+  plantedCrops: ['Soja', 'Milho', 'Algodão', 'Café', 'Cana de Açucar']
+})
+
+describe('UpdateRuralProducer Controller', () => {
+  test('Deve chamar Validation com os dados corretos', async () => {
+    const { sut, validationSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(validationSpy.input).toEqual(request)
+  })
+})
