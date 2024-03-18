fa75ddc79c490a2eecb254453dfb706214d7862a
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 10:25:36 -0300

    test: garante que DbDeletePlantedCrops retorne uma exceção caso DeletePlantedCropsRepository falhe

--- tests/data/usecases/db-delete-planted-crops.spec.ts
+++ tests/data/usecases/db-delete-planted-crops.spec.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { DbDeletePlantedCrops } from '@/data/usecases'
 import { DeletePlantedCropsRepositorySpy } from '@/tests/data/mocks'
+import { throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DbDeletePlantedCrops
@@ -22,5 +23,11 @@ describe('DbDeletePlantedCrops UseCase', () => {
     const farmId = faker.number.int({ min: 1, max: 100 })
     await sut.delete(farmId)
     expect(deletePlantedCropsRepositorySpy.farmId).toBe(farmId)
+  })
+  test('Deve retornar uma exceção caso DeletePlantedCropsRepository falhe', async () => {
+    const { sut, deletePlantedCropsRepositorySpy } = makeSut()
+    jest.spyOn(deletePlantedCropsRepositorySpy, 'delete').mockImplementationOnce(throwError)
+    const promise = sut.delete(faker.number.int({ min: 1, max: 100 }))
+    await expect(promise).rejects.toThrow()
   })
 })
