5d36ca7a65369c71ef5ed555771a26376a3c13f8
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 11:19:27 -0300

    test: garante que DashboardController retorne 500 caso LoadDashboard falhe

--- tests/presentation/controllers/dashboard-controller.spec.ts
+++ tests/presentation/controllers/dashboard-controller.spec.ts
@@ -1,5 +1,7 @@
-import { type LoadDashboard } from '@/domain/usecases'
 import { DashboardController } from '@/presentation/controllers'
+import { serverError } from '@/presentation/helpers'
+import { type LoadDashboard } from '@/domain/usecases'
+import { throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DashboardController
@@ -29,5 +31,11 @@ describe('Dashboard Controller', () => {
     const { sut, loadDashboardSpy } = makeSut()
     await sut.handle()
     expect(loadDashboardSpy.callsCount).toBe(1)
+  })
+  test('Deve retornar 500 caoso LoadDashboard falhe', async () => {
+    const { sut, loadDashboardSpy } = makeSut()
+    jest.spyOn(loadDashboardSpy, 'load').mockImplementationOnce(throwError)
+    const response = await sut.handle()
+    expect(response).toEqual(serverError(new Error()))
   })
 })
