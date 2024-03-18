5b09a2532236122575e282eea52f25ed6fa805ac
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 11:19:18 -0300

    feat: garante que DashboardController retorne 500 caso LoadDashboard falhe

--- src/presentation/controllers/dashboard-controller.ts
+++ src/presentation/controllers/dashboard-controller.ts
@@ -1,11 +1,16 @@
 import type { Controller, HttpResponse } from '@/presentation/protocols'
+import { serverError } from '@/presentation/helpers'
 import type { LoadDashboard } from '@/domain/usecases'
 
 export class DashboardController implements Controller {
   constructor (private readonly loadDashboard: LoadDashboard) {}
 
   async handle (): Promise<HttpResponse> {
-    await this.loadDashboard.load()
-    return null
+    try {
+      await this.loadDashboard.load()
+      return null
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
