1fd2b935b95472e6f5ac6324814901536dff304a
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 01:39:33 -0300

    feat: garante que uma cultura plantada rural seja atualizado em caso de sucesso

--- src/infra/db/postgres/planted-crops-postgres-repository.ts
+++ src/infra/db/postgres/planted-crops-postgres-repository.ts
@@ -1,11 +1,16 @@
 import { KnexHelper } from '@/infra/db/postgres/helpers'
-import type { AddPlantedCropsRepository } from '@/data/protocols'
+import type { AddPlantedCropsRepository, UpdatePlantedCropsRepository } from '@/data/protocols'
 
-export class PlantedCropsPostgresRepository implements AddPlantedCropsRepository {
+export class PlantedCropsPostgresRepository implements AddPlantedCropsRepository, UpdatePlantedCropsRepository {
   async add (params: AddPlantedCropsRepository.Params): Promise<void> {
     const insertParams = params.plantedCrops.map((plantedCrop) => ({ farm_id: params.farmId, name: plantedCrop }))
     await KnexHelper.client
       .insert(insertParams)
       .into('planted_crop')
+  }
+
+  async update (params: UpdatePlantedCropsRepository.Params): Promise<void> {
+    await KnexHelper.client.delete().from('planted_crop').where('farm_id', params.farmId)
+    await this.add(params)
   }
 }
