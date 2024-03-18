0691c6f00f9701ea3c5a651cf1c13a14c64cbe1a
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 01:39:51 -0300

    test: garante que uma cultura plantada rural seja atualizado em caso de sucesso

--- tests/domain/mocks/mock-planted-crops.ts
+++ tests/domain/mocks/mock-planted-crops.ts
@@ -6,4 +6,7 @@ export const mockAddPlantedCropsParams = (): AddPlantedCrops.Params => ({
   plantedCrops: ['Cana de açucar', 'Soja']
 })
 
-export const mockUpdatePlantedCropsParams = (): AddPlantedCrops.Params => mockAddPlantedCropsParams()
+export const mockUpdatePlantedCropsParams = (farmId = faker.number.int({ min: 1, max: 100 })): AddPlantedCrops.Params => ({
+  farmId,
+  plantedCrops: ['Cana de açucar', 'Soja', 'Milho']
+})
--- tests/infra/db/postgres/planted-crops-postgres-repository.spec.ts
+++ tests/infra/db/postgres/planted-crops-postgres-repository.spec.ts
@@ -1,6 +1,6 @@
 import { PlantedCropsPostgresRepository } from '@/infra/db/postgres'
 import { KnexHelper } from '@/infra/db/postgres/helpers'
-import { mockAddPlantedCropsParams } from '@/tests/domain/mocks'
+import { mockAddPlantedCropsParams, mockUpdatePlantedCropsParams } from '@/tests/domain/mocks'
 
 const makeSut = (): PlantedCropsPostgresRepository => {
   return new PlantedCropsPostgresRepository()
@@ -46,6 +46,34 @@ describe('PlantedCrops Postgres Repository', () => {
         farm_id: params2.farmId,
         name: params2.plantedCrops[1]
       }])
+    })
+  })
+  describe('update', () => {
+    test('Deve atualizar uma cultura plantada em caso de sucesso', async () => {
+      const sut = makeSut()
+      const params = mockAddPlantedCropsParams()
+      await sut.add(params)
+      let plantedCropsDatabase = await KnexHelper.client.select('*').from('planted_crop')
+      expect(plantedCropsDatabase).toHaveLength(2)
+      expect(plantedCropsDatabase[0].id).toBeGreaterThan(0)
+      expect(plantedCropsDatabase[0].farm_id).toBe(params.farmId)
+      expect(plantedCropsDatabase[0].name).toBe(params.plantedCrops[0])
+      expect(plantedCropsDatabase[1].id).toBeGreaterThan(0)
+      expect(plantedCropsDatabase[1].farm_id).toBe(params.farmId)
+      expect(plantedCropsDatabase[1].name).toBe(params.plantedCrops[1])
+      const updateParams = mockUpdatePlantedCropsParams(params.farmId)
+      await sut.update(updateParams)
+      plantedCropsDatabase = await KnexHelper.client.select('*').from('planted_crop')
+      expect(plantedCropsDatabase).toHaveLength(3)
+      expect(plantedCropsDatabase[0].id).toBeGreaterThan(0)
+      expect(plantedCropsDatabase[0].farm_id).toBe(updateParams.farmId)
+      expect(plantedCropsDatabase[0].name).toBe(updateParams.plantedCrops[0])
+      expect(plantedCropsDatabase[1].id).toBeGreaterThan(0)
+      expect(plantedCropsDatabase[1].farm_id).toBe(updateParams.farmId)
+      expect(plantedCropsDatabase[1].name).toBe(updateParams.plantedCrops[1])
+      expect(plantedCropsDatabase[2].id).toBeGreaterThan(0)
+      expect(plantedCropsDatabase[2].farm_id).toBe(updateParams.farmId)
+      expect(plantedCropsDatabase[2].name).toBe(updateParams.plantedCrops[2])
     })
   })
 })
