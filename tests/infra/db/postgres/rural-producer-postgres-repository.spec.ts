c47b4741335a916b72de895caca103ec4fd30ace
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 01:18:44 -0300

    test: garante que um produtor rural seja atualizado em caso de sucesso

--- tests/infra/db/postgres/rural-producer-postgres-repository.spec.ts
+++ tests/infra/db/postgres/rural-producer-postgres-repository.spec.ts
@@ -1,6 +1,6 @@
 import { RuralProducerPostgresRepository } from '@/infra/db/postgres'
 import { KnexHelper } from '@/infra/db/postgres/helpers'
-import { mockAddRuralProducerParams } from '@/tests/domain/mocks'
+import { mockAddRuralProducerParams, mockUpdateRuralProducerParams } from '@/tests/domain/mocks'
 
 const makeSut = (): RuralProducerPostgresRepository => {
   return new RuralProducerPostgresRepository()
@@ -29,6 +29,26 @@ describe('RuralProducer Postgres Repository', () => {
       expect(newRuralProducerId).toBeGreaterThan(0)
       expect(newRuralProducerId2).toBe(newRuralProducerId + 1)
       expect(newRuralProducerId3).toBe(newRuralProducerId2 + 1)
+    })
+  })
+
+  describe('update', () => {
+    test('Deve atualizar um produtor rural em caso de sucesso', async () => {
+      const sut = makeSut()
+      const addParams = mockAddRuralProducerParams()
+      const newRuralProducerId = await sut.add(addParams)
+      let ruralProducerDatabase = await KnexHelper.client.select('*').from('rural_producer')
+      expect(ruralProducerDatabase).toHaveLength(1)
+      expect(ruralProducerDatabase[0].id).toBe(newRuralProducerId)
+      expect(ruralProducerDatabase[0].cpf_cnpj).toBe(addParams.cpfCnpj)
+      expect(ruralProducerDatabase[0].name).toBe(addParams.name)
+      const updateParams = mockUpdateRuralProducerParams(newRuralProducerId)
+      await sut.update(updateParams)
+      ruralProducerDatabase = await KnexHelper.client.select('*').from('rural_producer')
+      expect(ruralProducerDatabase).toHaveLength(1)
+      expect(ruralProducerDatabase[0].id).toBe(newRuralProducerId)
+      expect(ruralProducerDatabase[0].cpf_cnpj).toBe(updateParams.cpfCnpj)
+      expect(ruralProducerDatabase[0].name).toBe(updateParams.name)
     })
   })
 })
