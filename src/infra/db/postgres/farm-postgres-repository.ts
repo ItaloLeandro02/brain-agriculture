834baf8a0804bbdbe4b758676f2a6f7532d6247b
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 01:56:24 -0300

    test: garante que uma fazenda seja atualizado em caso de sucesso

--- src/infra/db/postgres/farm-postgres-repository.ts
+++ src/infra/db/postgres/farm-postgres-repository.ts
@@ -1,7 +1,7 @@
 import { KnexHelper } from '@/infra/db/postgres/helpers'
-import type { AddFarmRepository } from '@/data/protocols'
+import type { AddFarmRepository, UpdateFarmRepository } from '@/data/protocols'
 
-export class FarmPostgresRepository implements AddFarmRepository {
+export class FarmPostgresRepository implements AddFarmRepository, UpdateFarmRepository {
   async add (params: AddFarmRepository.Params): Promise<number> {
     const [result] = await KnexHelper.client
       .insert({
@@ -14,6 +14,22 @@ export class FarmPostgresRepository implements AddFarmRepository {
         vegetation_area: params.vegetationArea
       })
       .into('farm')
+      .returning('id')
+    return result.id
+  }
+
+  async update (params: UpdateFarmRepository.Params): Promise<number> {
+    const [result] = await KnexHelper.client
+      .update({
+        name: params.name,
+        city_name: params.cityName,
+        state: params.state,
+        total_area: params.totalArea,
+        agricultural_area: params.agriculturalArea,
+        vegetation_area: params.vegetationArea
+      })
+      .from('farm')
+      .where('rural_producer_id', params.ruralProducerId)
       .returning('id')
     return result.id
   }
