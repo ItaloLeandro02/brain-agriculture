25489fe3c7b0a54f60c87f6b8f5c1dd4e5aa8365
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 02:27:39 -0300

    test: garante que ao atualizar um produtor rural seja retornado 204

--- tests/main/routes/rural-producer-routes.test.ts
+++ tests/main/routes/rural-producer-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import type { Express } from 'express'
 import { setupApp } from '@/main/config'
 import { KnexHelper } from '@/infra/db/postgres/helpers'
-import { mockAddRuralProducerParams, mockAddFarmParams, mockAddPlantedCropsParams } from '@/tests/domain/mocks'
+import { mockAddRuralProducerParams, mockAddFarmParams, mockAddPlantedCropsParams, mockUpdatePlantedCropsParams } from '@/tests/domain/mocks'
 
 let app: Express
 
@@ -22,18 +22,68 @@ describe('RuralProducer Routes', () => {
     await KnexHelper.client.delete().from('rural_producer')
   })
 
-  test('Deve retornar 204 em caso de sucesso', async () => {
-    const { ruralProducerId, name: farmName, ...farmParams } = mockAddFarmParams()
-    const { plantedCrops } = mockAddPlantedCropsParams()
-    const params = {
-      ...mockAddRuralProducerParams(),
-      ...farmParams,
-      farmName,
-      plantedCrops
-    }
-    await request(app)
-      .post('/api/rural-producer')
-      .send(params)
-      .expect(204)
+  describe('POST /rural-producer', () => {
+    test('Deve retornar 204 em caso de sucesso', async () => {
+      const { ruralProducerId, name: farmName, ...farmParams } = mockAddFarmParams()
+      const { plantedCrops } = mockAddPlantedCropsParams()
+      const params = {
+        ...mockAddRuralProducerParams(),
+        ...farmParams,
+        farmName,
+        plantedCrops
+      }
+      await request(app)
+        .post('/api/rural-producer')
+        .send(params)
+        .expect(204)
+    })
+  })
+  describe('PUT /rural-producer/:id', () => {
+    test('Deve retornar 204 em caso de sucesso', async () => {
+      const addRuralProducerParams = mockAddRuralProducerParams()
+      const [{ id: ruralProducerId }] = await KnexHelper.client
+        .insert({
+          name: addRuralProducerParams.name,
+          cpf_cnpj: addRuralProducerParams.cpfCnpj
+        })
+        .into('rural_producer')
+        .returning('id')
+      const addFarmParams = mockAddFarmParams()
+      const [{ id: farmId }] = await KnexHelper.client
+        .insert({
+          rural_producer_id: ruralProducerId,
+          name: addFarmParams.name,
+          city_name: addFarmParams.cityName,
+          state: addFarmParams.state,
+          total_area: addFarmParams.totalArea,
+          agricultural_area: addFarmParams.agriculturalArea,
+          vegetation_area: addFarmParams.vegetationArea
+        })
+        .into('farm')
+        .returning('id')
+      const addPlantedCropsParams = mockAddPlantedCropsParams()
+      await KnexHelper.client
+        .insert([{
+          farm_id: farmId,
+          name: addPlantedCropsParams.plantedCrops[0]
+        }, {
+          farm_id: farmId,
+          name: addPlantedCropsParams.plantedCrops[1]
+        }])
+        .into('planted_crop')
+
+      const { name: farmName, ...farmParams } = mockAddFarmParams()
+      const { plantedCrops } = mockUpdatePlantedCropsParams()
+      const params = {
+        ...mockAddRuralProducerParams(),
+        ...farmParams,
+        farmName,
+        plantedCrops
+      }
+      await request(app)
+        .put(`/api/rural-producer/${ruralProducerId}`)
+        .send(params)
+        .expect(204)
+    })
   })
 })
