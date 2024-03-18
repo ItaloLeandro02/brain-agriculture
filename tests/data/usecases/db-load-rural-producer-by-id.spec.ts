720c4a47373adae4f1b14a0ceedb055bfeda5ad4
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 03:02:19 -0300

    test: garante que DbLoadRuralProducerById retorne uma exceção caso o repository falhe

--- tests/data/usecases/db-load-rural-producer-by-id.spec.ts
+++ tests/data/usecases/db-load-rural-producer-by-id.spec.ts
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { DbLoadRuralProducerById } from '@/data/usecases'
 import { LoadRuralProducerByIdRepositorySpy } from '@/tests/data/mocks'
+import { throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DbLoadRuralProducerById
@@ -22,5 +23,11 @@ describe('DbLoadRuralProducerById UseCase', () => {
     const ruralProducerId = faker.number.int({ min: 1, max: 100 })
     await sut.load(ruralProducerId)
     expect(loadRuralProducerByIdRepositorySpy.id).toEqual(ruralProducerId)
+  })
+  test('Deve lançar uma exceção caso LoadRuralProducerByIdRepository lance uma exceção', async () => {
+    const { sut, loadRuralProducerByIdRepositorySpy } = makeSut()
+    jest.spyOn(loadRuralProducerByIdRepositorySpy, 'load').mockImplementationOnce(throwError)
+    const promise = sut.load(faker.number.int({ min: 1, max: 100 }))
+    await expect(promise).rejects.toThrow()
   })
 })
