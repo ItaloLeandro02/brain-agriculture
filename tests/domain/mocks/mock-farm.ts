6c93cccc1b60510e555b4149d2784c045a8a05c6
Italo Leandro <italo_leandro@outlook.com>
2024-03-18 01:56:35 -0300

    test: garante que uma fazenda seja atualizado em caso de sucesso

--- tests/domain/mocks/mock-farm.ts
+++ tests/domain/mocks/mock-farm.ts
@@ -11,4 +11,12 @@ export const mockAddFarmParams = (): AddFarm.Params => ({
   vegetationArea: 50
 })
 
-export const mockUpdateFarmParams = (): UpdateFarm.Params => mockAddFarmParams()
+export const mockUpdateFarmParams = (ruralProducerId = faker.number.int({ min: 1, max: 100 })): UpdateFarm.Params => ({
+  ruralProducerId,
+  name: faker.company.name(),
+  cityName: faker.location.city(),
+  state: 'MG',
+  totalArea: 300,
+  agriculturalArea: 250,
+  vegetationArea: 50
+})
--- tests/infra/db/postgres/farm-postgres-repository.spec.ts
+++ tests/infra/db/postgres/farm-postgres-repository.spec.ts
@@ -1,6 +1,6 @@
 import { FarmPostgresRepository } from '@/infra/db/postgres'
 import { KnexHelper } from '@/infra/db/postgres/helpers'
-import { mockAddFarmParams } from '@/tests/domain/mocks'
+import { mockAddFarmParams, mockUpdateFarmParams } from '@/tests/domain/mocks'
 
 const makeSut = (): FarmPostgresRepository => {
   return new FarmPostgresRepository()
@@ -29,6 +29,36 @@ describe('Farm Postgres Repository', () => {
       expect(newFarm).toBeGreaterThan(0)
       expect(newFarm2).toBe(newFarm + 1)
       expect(newFarm3).toBe(newFarm2 + 1)
+    })
+  })
+  describe('update', () => {
+    test('Deve atualizar uma fazenda em caso de sucesso', async () => {
+      const sut = makeSut()
+      const addParams = mockAddFarmParams()
+      const newFarmId = await sut.add(addParams)
+      let farmDatabase = await KnexHelper.client.select('*').from('farm')
+      expect(farmDatabase).toHaveLength(1)
+      expect(farmDatabase[0].id).toBeGreaterThan(0)
+      expect(farmDatabase[0].rural_producer_id).toBe(addParams.ruralProducerId)
+      expect(farmDatabase[0].name).toBe(addParams.name)
+      expect(farmDatabase[0].city_name).toBe(addParams.cityName)
+      expect(farmDatabase[0].state).toBe(addParams.state)
+      expect(parseFloat(farmDatabase[0].total_area)).toBe(addParams.totalArea)
+      expect(parseFloat(farmDatabase[0].agricultural_area)).toBe(addParams.agriculturalArea)
+      expect(parseFloat(farmDatabase[0].vegetation_area)).toBe(addParams.vegetationArea)
+      const updateParams = mockUpdateFarmParams(addParams.ruralProducerId)
+      const updatedFarmId = await sut.update(updateParams)
+      farmDatabase = await KnexHelper.client.select('*').from('farm')
+      expect(updatedFarmId).toBe(newFarmId)
+      expect(farmDatabase).toHaveLength(1)
+      expect(farmDatabase[0].id).toBeGreaterThan(0)
+      expect(farmDatabase[0].rural_producer_id).toBe(updateParams.ruralProducerId)
+      expect(farmDatabase[0].name).toBe(updateParams.name)
+      expect(farmDatabase[0].city_name).toBe(updateParams.cityName)
+      expect(farmDatabase[0].state).toBe(updateParams.state)
+      expect(parseFloat(farmDatabase[0].total_area)).toBe(updateParams.totalArea)
+      expect(parseFloat(farmDatabase[0].agricultural_area)).toBe(updateParams.agriculturalArea)
+      expect(parseFloat(farmDatabase[0].vegetation_area)).toBe(updateParams.vegetationArea)
     })
   })
 })
