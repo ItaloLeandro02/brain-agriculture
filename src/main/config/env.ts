c482bf9b3a2a4dd38ff926f6eaa35296855fe742
Italo Leandro <italo_leandro@outlook.com>
2024-03-19 11:50:45 -0300

    feat: adiciona console.log para debugar conexão com o banco em memória

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config'
 
+console.log(process.env.NODE_ENV)
+console.log(process.env.POSTGRES_URL)
+
 export const env = {
   port: process.env.PORT,
   postgresUrl: process.env.NODE_ENV === 'test' ? process.env.POSTGRES_URL_TEST : process.env.POSTGRES_URL
