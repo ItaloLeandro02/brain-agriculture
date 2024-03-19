3047f76ba8377cf273ab309496229d0c3e4d7daa
Italo Leandro <italo_leandro@outlook.com>
2024-03-19 11:59:35 -0300

    refactor: desfaz alterações para o GitHub Actions

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,5 @@
 import 'dotenv/config'
 
-console.log(process.env.NODE_ENV)
-console.log(process.env.POSTGRES_URL)
-
 export const env = {
   port: process.env.PORT,
   postgresUrl: process.env.NODE_ENV === 'test' ? process.env.POSTGRES_URL_TEST : process.env.POSTGRES_URL
