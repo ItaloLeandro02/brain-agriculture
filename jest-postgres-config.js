a2efe8be7e4185842779bebddd10fb9543bba9a1
Italo Leandro <italo_leandro@outlook.com>
2024-03-19 11:36:58 -0300

    fix: desfaz alterações na configuração do arquivo de teste do postgres

--- jest-postgres-config.js
+++ jest-postgres-config.js
@@ -4,6 +4,6 @@ module.exports = {
   seedPath: `${cwd()}/tests/schema.sql`,
   version: 14,
   port: 5555,
-	includeInstallation: true,
-  debugMode: true
+	includeInstallation: false,
+  debugMode: false
 };
